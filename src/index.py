0661c2c7c6cf9b46a56be14f326dc6b3da4774c3
Dawid <dawid87872@gmail.com>
2024-03-04 13:05:39 +0100

    Use server side sessions

--- src/index.py
+++ src/index.py
@@ -1,8 +1,15 @@
 from typing import Any
+from secrets import token_urlsafe
 from flask import Flask
+from flask_session import Session as ServerSideSession
 
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = token_urlsafe(16)
+
+ServerSideSession(app)
 
 
 @app.route("/")
