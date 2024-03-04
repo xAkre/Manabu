7a8a0624301daf88ffcacfea36404c4812b1680c
Dawid <dawid87872@gmail.com>
2024-03-04 10:25:25 +0100

    Rewrite test fixtures to use tmp_path

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from os import path, makedirs, remove, rmdir
 from uuid import uuid4
 from database import reset_database
 
@@ -13,33 +12,15 @@ def _reset_database():
 
 
 @pytest.fixture
-def temporary_databases_folder() -> str:
-    """
-    Create a temporary databases folder and return the path to it.
-    The folder will be removed after the test has finished
-
-    :return: The path to the generated temporary databases folder
-    """
-    temporary_databases_folder_path = path.join(path.dirname(__file__), "temporary_databases")
-    makedirs(temporary_databases_folder_path, exist_ok=True)
-
-    yield temporary_databases_folder_path
-
-    rmdir(temporary_databases_folder_path)
-
-
-@pytest.fixture
-def temporary_database_path(temporary_databases_folder) -> str:
+def temporary_database_path(tmp_path) -> str:
     """
     Generate a temporary database file and return the path to it.
     The database file will be removed after the test has finished
 
-    :param temporary_databases_folder: The path to the temporary databases folder
+    :param tmp_path: The path to the temporary directory provided by pytest
     :return: The path to the generated temporary database
     """
-    temporary_database_path = path.join(temporary_databases_folder, f"{uuid4()}.sqlite")
-    open(temporary_database_path, "w").close()
+    temporary_database_path = tmp_path / f"{uuid4()}.sqlite"
+    temporary_database_path.touch()
 
-    yield temporary_database_path
-
-    remove(temporary_database_path)
+    return str(temporary_database_path)
