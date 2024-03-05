17a739c38b4eb0d6ae29308bab7182dfadde3ca8
Dawid <dawid87872@gmail.com>
2024-03-05 19:10:24 +0100

    Remove database session after every request

--- src/index.py
+++ src/index.py
@@ -1,7 +1,9 @@
 from secrets import token_urlsafe
 from flask import Flask
 from flask_session import Session as ServerSideSession
+from database import session, set_database
 from routes import general_router, auth_router
+from config import Config
 
 
 app = Flask(__name__)
@@ -13,3 +15,14 @@ ServerSideSession(app)
 
 app.register_blueprint(general_router)
 app.register_blueprint(auth_router)
+
+set_database(Config.DATABASE_URL)
+
+
+@app.teardown_appcontext
+def shutdown_session(exc=None) -> None: # noqa: Suppress "Parameter 'exc' value is not used". Flask requires the parameter to be there
+    """
+    Rollback any uncommitted changes and remove the current session after every request
+    """
+    session.rollback()
+    session.remove()
