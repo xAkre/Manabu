fb84bb66cdbe86d84bcab67f891dd26fc8a1567a
Dawid <dawid87872@gmail.com>
2024-03-05 14:04:29 +0100

    Implement registering logic server side and create tests

--- src/routes/auth/register.py
+++ src/routes/auth/register.py
@@ -1,8 +1,51 @@
 from typing import Any
+from http import HTTPStatus
+from flask import request, abort
+from utils.flask import FieldNotFoundError, form_require
+from database import session
+from database.orm import select
+from database.exc import DatabaseError
+from database.models import User
+from utils.security import hash_password
 
 
 def register() -> Any:
     """
-    Handles registering
+    Handles the /register/ route
     """
-    return "Register"
+    if request.method == "GET":
+        return "Register"
+
+    # Validate input
+    try:
+        username = form_require("username", "Username is required")
+        email = form_require("email", "Email is required")
+        password = form_require("password", "Password is required")
+        password_confirmation = form_require("password_confirmation", "Password confirmation is required")
+    except FieldNotFoundError as e:
+        return e.message, HTTPStatus.BAD_REQUEST
+
+    if not password_confirmation == password:
+        return "Password confirmation does not match password", HTTPStatus.BAD_REQUEST
+
+    # Check if user has not already registered before
+    existing_user = session.execute(select(User).where(User.email == email)).scalar()
+
+    if existing_user is not None:
+        return "This email already exists", HTTPStatus.BAD_REQUEST
+
+    # Add the user to the database
+    hashed_password, salt = hash_password(password)
+
+    try:
+        new_user = User(
+            username=username,
+            email=email,
+            hashed_password=hashed_password,
+            salt=salt
+        )
+        session.add(new_user)
+        session.commit()
+        return "Added successfully", HTTPStatus.CREATED
+    except DatabaseError:
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR)
--- src/tests/test_routes/test_auth_router.py
+++ src/tests/test_routes/test_auth_router.py
@@ -1,5 +1,9 @@
+import pytest
 from http import HTTPStatus
 from flask import url_for
+from database import session
+from database.orm import select
+from database.models import User
 from routes import auth_router
 
 
@@ -27,3 +31,78 @@ def test_can_get_register_page(flask_app) -> None:
     with flask_app.app_context(), flask_app.test_request_context():
         response = test_client.get(url_for("auth.register"))
         assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_can_register_user(flask_app) -> None:
+    """
+    Make sure that the /register/ route can successfully register a user
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context():
+        response = test_client.post(url_for("auth.register"), data={
+            "username": "username",
+            "email": "email@email.com",
+            "password": "password",
+            "password_confirmation": "password"
+        })
+        assert response.status_code == HTTPStatus.CREATED
+
+        user = session.execute(select(User).where(User.email == "email@email.com")).scalar()
+        assert user is not None
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_trying_to_register_user_twice_returns_bad_request(flask_app) -> None:
+    """
+    Make sure that the /register/ route returns a bad request status code when trying to register
+    a user twice
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context():
+        test_client.post(url_for("auth.register"), data={
+            "username": "username",
+            "email": "email@email.com",
+            "password": "password",
+            "password_confirmation": "password"
+        })
+        response = test_client.post(url_for("auth.register"), data={
+            "username": "username",
+            "email": "email@email.com",
+            "password": "password",
+            "password_confirmation": "password"
+        })
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_trying_to_register_with_invalid_password_confirmation_returns_bad_request(flask_app) -> None:
+    """
+    Make sure that the /register/ route returns a bad request status code when the password confirmation does not
+    match the password
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context():
+        response = test_client.post(url_for("auth.register"), data={
+            "username": "username",
+            "email": "email@email.com",
+            "password": "password",
+            "password_confirmation": "some_other_password"
+        })
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
