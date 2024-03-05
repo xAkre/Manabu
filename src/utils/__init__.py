cc308a639cbd18345d76687d72e728fac91f6443
Dawid <dawid87872@gmail.com>
2024-03-05 11:38:31 +0100

    Add flask helper function

---
+++ src/utils/__init__.py
@@ -0,0 +1,3 @@
+"""
+This package contains utility functions and classes for the application
+"""
---
+++ src/utils/flask/__init__.py
@@ -0,0 +1,33 @@
+"""
+This package contains a utility class for flask
+"""
+from flask import request
+
+
+class FieldNotFoundError(Exception):
+    """
+    This error is raised when a field is not found in a flask object
+    """
+
+    def __init__(self, message: str):
+        """
+        This error is raised when a field is not found in a flask object
+
+        :param message: The error's message
+        """
+        self.message = message
+
+
+def form_require(field: str, message: str = "") -> str:
+    """
+    This function is used to require a field in flask's request's form field. If the field is present, it will
+    be returned. If it is not, a KeyError will be thrown
+
+    :param field: The field that is required
+    :param message: Optional message that will be passed to the KeyError if it is thrown
+    :raises FieldNotFoundError: When the field is not present
+    """
+    if request.form.get(field):
+        return request.form.get(field)
+
+    raise FieldNotFoundError(message)
