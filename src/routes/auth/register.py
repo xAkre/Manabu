6d7e7afa515c80a6b7233a345dfcfe4b23760b73
Dawid <dawid87872@gmail.com>
2024-03-06 10:00:45 +0100

    Redirect user on successful register and flash appropriate messages

--- src/routes/auth/register.py
+++ src/routes/auth/register.py
@@ -1,6 +1,6 @@
 from typing import Any
 from http import HTTPStatus
-from flask import request, abort
+from flask import request, redirect, abort, flash, url_for
 from utils.flask import FieldNotFoundError, form_require
 from database import session
 from database.orm import select
@@ -25,15 +25,18 @@ def register() -> Any:
         password = form_require("password", "Password is required")
         password_confirmation = form_require("password_confirmation", "Password confirmation is required")
     except FieldNotFoundError as e:
+        flash(e.message, "error")
         return e.message, HTTPStatus.BAD_REQUEST
 
     if not password_confirmation == password:
+        flash("Password confirmation does not match password", "error")
         return "Password confirmation does not match password", HTTPStatus.BAD_REQUEST
 
     # Check if user has not already registered before
     existing_user = session.execute(select(User).where(User.email == email)).scalar()
 
     if existing_user is not None:
+        flash("This email already exists", "error")
         return "This email already exists", HTTPStatus.BAD_REQUEST
 
     # Add the user to the database
@@ -48,6 +51,8 @@ def register() -> Any:
         )
         session.add(new_user)
         session.commit()
-        return "Added successfully", HTTPStatus.CREATED
+        flash("Successfully registered", "success")
+        return redirect(url_for("auth.login")), HTTPStatus.CREATED
     except DatabaseError:
+        flash("There was an error while trying to register", "error")
         abort(HTTPStatus.INTERNAL_SERVER_ERROR)
