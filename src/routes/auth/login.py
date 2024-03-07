0f29afc52fef099a1904af3182da1249e6c62474
Dawid <dawid87872@gmail.com>
2024-03-07 06:35:15 +0100

    Implement login logic server side and add tests

--- src/routes/auth/login.py
+++ src/routes/auth/login.py
@@ -1,5 +1,13 @@
 from typing import Any
+from http import HTTPStatus
+from flask import request, flash, redirect, url_for, session as f_session
+from database import session as d_session
+from database.orm import select, or_
+from database.exc import DatabaseError
+from database.models import User
 from middleware import redirect_if_logged_in
+from utils.flask import FieldNotFoundError, form_require
+from utils.security import check_password
 
 
 @redirect_if_logged_in
@@ -7,4 +15,36 @@ def login() -> Any:
     """
     Handles the /login/ route
     """
-    return "Login"
+    if request.method == "GET":
+        return "Login"
+
+    # Validate user input
+    try:
+        username_or_email = form_require("username_or_email", "Username or email is required")
+        password = form_require("password", "Password is required")
+    except FieldNotFoundError as e:
+        flash(e.message, "error")
+        return e.message, HTTPStatus.BAD_REQUEST
+
+    # Check if the email or username exists
+    try:
+        user = d_session.execute(
+            select(User).where(or_(User.email == username_or_email, User.username == username_or_email))).scalar()
+    except DatabaseError as e:
+        flash("There was an error while trying to login", "error")
+        return "There was an error while trying to login", HTTPStatus.INTERNAL_SERVER_ERROR
+
+    if user is None:
+        flash("There is no user with that username or email", "error")
+        return "There is no user with that username or email", HTTPStatus.BAD_REQUEST
+
+    # Check if the password matches
+    if not check_password(password, user.salt, user.hashed_password):
+        flash("Incorrect password", "error")
+        return "Incorrect password", HTTPStatus.BAD_REQUEST
+
+    # Log the user in
+    f_session.update({"user": user})
+    flash("Successfully registered", "success")
+    return redirect("/dashboard"), HTTPStatus.OK
+
--- src/tests/test_routes/test_auth_router.py
+++ src/tests/test_routes/test_auth_router.py
@@ -1,7 +1,7 @@
 import pytest
 from http import HTTPStatus
-from flask import url_for
-from database import session
+from flask import url_for, session as f_session
+from database import session as d_session
 from database.orm import select
 from database.models import User
 from routes import auth_router
@@ -44,15 +44,20 @@ def test_can_register_user(flask_app) -> None:
     test_client = flask_app.test_client()
 
     with flask_app.app_context(), flask_app.test_request_context():
-        response = test_client.post(url_for("auth.register"), data={
-            "username": "username",
-            "email": "email@email.com",
-            "password": "password",
-            "password_confirmation": "password"
-        })
+        response = test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
         assert response.status_code == HTTPStatus.CREATED
 
-        user = session.execute(select(User).where(User.email == "email@email.com")).scalar()
+        user = d_session.execute(
+            select(User).where(User.email == "email@email.com")
+        ).scalar()
         assert user is not None
 
 
@@ -68,24 +73,32 @@ def test_trying_to_register_user_twice_returns_bad_request(flask_app) -> None:
     test_client = flask_app.test_client()
 
     with flask_app.app_context(), flask_app.test_request_context():
-        test_client.post(url_for("auth.register"), data={
-            "username": "username",
-            "email": "email@email.com",
-            "password": "password",
-            "password_confirmation": "password"
-        })
-        response = test_client.post(url_for("auth.register"), data={
-            "username": "username",
-            "email": "email@email.com",
-            "password": "password",
-            "password_confirmation": "password"
-        })
+        test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
+        response = test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
 
         assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
 @pytest.mark.usefixtures("set_temporary_database")
-def test_trying_to_register_with_invalid_password_confirmation_returns_bad_request(flask_app) -> None:
+def test_trying_to_register_with_invalid_password_confirmation_returns_bad_request(
+    flask_app,
+) -> None:
     """
     Make sure that the /register/ route returns a bad request status code when the password confirmation does not
     match the password
@@ -96,13 +109,185 @@ def test_trying_to_register_with_invalid_password_confirmation_returns_bad_reque
     test_client = flask_app.test_client()
 
     with flask_app.app_context(), flask_app.test_request_context():
-        response = test_client.post(url_for("auth.register"), data={
-            "username": "username",
-            "email": "email@email.com",
-            "password": "password",
-            "password_confirmation": "some_other_password"
-        })
+        response = test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "some_other_password",
+            },
+        )
 
         assert response.status_code == HTTPStatus.BAD_REQUEST
 
 
+def test_url_for_login_page(flask_app) -> None:
+    """
+    Make sure that url for auth.login returns "/login/"
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+
+    with flask_app.app_context(), flask_app.test_request_context():
+        assert url_for("auth.login") == "/login/"
+
+
+def test_can_get_login_page(flask_app) -> None:
+    """
+    Make sure that the login page can be fetched
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context():
+        response = test_client.get(url_for("auth.login"))
+        assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_can_log_in_with_username(flask_app) -> None:
+    """
+    Make sure that the /login/ route can successfully log a user in with their username
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context(), test_client:
+        test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
+        response = test_client.post(
+            url_for("auth.login"),
+            data={"username_or_email": "username", "password": "password"},
+        )
+        user = d_session.execute(
+            select(User).where(User.username == "username")
+        ).scalar()
+
+        assert response.status_code == HTTPStatus.OK
+        assert user is not None
+        assert f_session.get("user") == user
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_can_log_in_with_email(flask_app) -> None:
+    """
+    Make sure that the /login/ route can successfully log a user in with their email
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context(), test_client:
+        test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
+        response = test_client.post(
+            url_for("auth.login"),
+            data={"username_or_email": "email@email.com", "password": "password"},
+        )
+        user = d_session.execute(
+            select(User).where(User.email == "email@email.com")
+        ).scalar()
+
+        assert response.status_code == HTTPStatus.OK
+        assert user is not None
+        assert f_session.get("user") == user
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_cannot_log_in_with_a_username_or_email_that_does_not_exist(flask_app) -> None:
+    """
+    Make sure that you cannot log in with a username or email that does not exist
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context(), test_client:
+        response = test_client.post(
+            url_for("auth.login"),
+            data={"username_or_email": "does_not_exist", "password": "password"},
+        )
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_cannot_log_in_with_a_correct_username_but_incorrect_password(
+    flask_app,
+) -> None:
+    """
+    Make sure that you cannot log in with a password that doesn't match the username
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context(), test_client:
+        test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
+        response = test_client.post(
+            url_for("auth.login"),
+            data={"username_or_email": "username", "password": "incorrect_password"},
+        )
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+
+@pytest.mark.usefixtures("set_temporary_database")
+def test_cannot_log_in_with_a_correct_email_but_incorrect_password(
+    flask_app,
+) -> None:
+    """
+    Make sure that you cannot log in with a password that doesn't match the email
+
+    :param flask_app: A flask application
+    """
+    flask_app.register_blueprint(auth_router)
+    test_client = flask_app.test_client()
+
+    with flask_app.app_context(), flask_app.test_request_context(), test_client:
+        test_client.post(
+            url_for("auth.register"),
+            data={
+                "username": "username",
+                "email": "email@email.com",
+                "password": "password",
+                "password_confirmation": "password",
+            },
+        )
+        response = test_client.post(
+            url_for("auth.login"),
+            data={
+                "username_or_email": "email@email.com",
+                "password": "incorrect_password",
+            },
+        )
+        assert response.status_code == HTTPStatus.BAD_REQUEST
