a87e12404eb4ce02bacf9562c0a3cf378a180d6c
Dawid <dawid87872@gmail.com>
2024-03-25 05:32:26 +0100

    Create a helper function for testing
    Create a helper function that registers and logs in a user

--- src/tests/test_routes/test_categories_router.py
+++ src/tests/test_routes/test_categories_router.py
@@ -1,12 +1,11 @@
 import pytest
 from http import HTTPStatus
-from flask import Flask, url_for
-from flask.testing import FlaskClient
+from flask import url_for
 from database import session as d_session
 from database.orm import select
 from database.models import User
-from routes import categories_router, auth_router, general_router
-from tests.utils import form_data
+from routes import categories_router
+from tests.utils import form_data, register_and_login
 
 
 def test_url_for_add_category_page(flask_app) -> None:
@@ -32,7 +31,7 @@ def test_can_get_add_category_page(flask_app) -> None:
     test_client = flask_app.test_client()
 
     with flask_app.test_request_context():
-        _register_and_login_test_user(flask_app, test_client)
+        register_and_login(test_client, flask_app=flask_app)
         response = test_client.get(url_for("categories.create"))
         assert response.status_code == HTTPStatus.OK
 
@@ -48,7 +47,7 @@ def test_can_add_category(flask_app) -> None:
     test_client = flask_app.test_client()
 
     with flask_app.test_request_context():
-        _register_and_login_test_user(flask_app, test_client)
+        register_and_login(test_client, flask_app=flask_app)
         data = form_data({"name": "work", "color": "#FFFFFF"})
         response = test_client.post(url_for("categories.create"), data=data)
         assert response.status_code == HTTPStatus.CREATED
@@ -70,10 +69,7 @@ def test_cant_add_category_with_invalid_color(flask_app) -> None:
     test_client = flask_app.test_client()
 
     with flask_app.test_request_context():
-        _register_and_login_test_user(
-            flask_app,
-            test_client,
-        )
+        register_and_login(test_client, flask_app=flask_app)
         data = form_data({"name": "work", "color": "#FFFFF"})
         response = test_client.post(url_for("categories.create"), data=data)
         assert response.status_code == HTTPStatus.BAD_REQUEST
@@ -105,42 +101,8 @@ def test_can_show_categories(flask_app) -> None:
     test_client = flask_app.test_client()
 
     with flask_app.test_request_context():
-        _register_and_login_test_user(flask_app, test_client)
+        register_and_login(test_client, flask_app=flask_app)
         data = form_data({"name": "work", "color": "#FFFFFF"})
         test_client.post(url_for("categories.create"), data=data)
         response = test_client.get(url_for("categories.show"))
         assert response.status_code == HTTPStatus.OK
-
-
-def _register_and_login_test_user(flask_app: Flask, test_client: FlaskClient) -> None:
-    """
-    Register and login a user with the following fields:
-
-    - username: username
-    - email: email@email.com
-    - password: Password_123
-
-    This function registers the auth and general routers. Make sure that this function is called inside a
-    test request context so that calls to url for can be resolved
-
-    :param flask_app: The flask app to register the blueprints on
-    :param test_client: The flask test client to send the data to
-    """
-    if not flask_app.blueprints.get("auth"):
-        flask_app.register_blueprint(auth_router)
-    if not flask_app.blueprints.get("general"):
-        flask_app.register_blueprint(general_router)
-
-    register_form_data = form_data(
-        {
-            "username": "username",
-            "email": "email@email.com",
-            "password": "Password_123",
-            "password_confirmation": "Password_123",
-        }
-    )
-    test_client.post(url_for("auth.register"), data=register_form_data)
-    login_form_data = form_data(
-        {"username_or_email": "username", "password": "Password_123"}
-    )
-    test_client.post(url_for("auth.login"), data=login_form_data)
--- src/tests/utils/__init__.py
+++ src/tests/utils/__init__.py
@@ -3,6 +3,9 @@ This package contains utility functions for testing
 """
 
 from multidict import MultiDict
+from flask import Flask, url_for
+from flask.testing import FlaskClient
+from routes import general_router, auth_router
 
 
 def form_data(dictionary: dict) -> MultiDict:
@@ -13,3 +16,41 @@ def form_data(dictionary: dict) -> MultiDict:
     """
 
     return MultiDict(dictionary)
+
+
+def register_and_login(
+    test_client: FlaskClient,
+    flask_app: Flask | None = None,
+    username: str = "username",
+    email: str = "email@email.com",
+    password: str = "Password_123",
+) -> dict:
+    """
+    Register and login a user using the application's routes. This function registers the auth and general routers
+    if a flask application is passed as an argument. Make sure this function is called inside a test request
+    context so that calls to url_for can be resolved
+
+    :param test_client: The test client to post the data to
+    :param flask_app: The flask app to register the required blueprints on. Defaults to None
+    :param username: The user's username. Defaults to "username"
+    :param email: The user's email. Defaults to "email@email.com"
+    :param password: The user's password. Defaults to "Password_123"
+    :return: The user's data as a dictionary
+    """
+    if flask_app:
+        flask_app.register_blueprint(auth_router)
+        flask_app.register_blueprint(general_router)
+
+    register_form_data = form_data(
+        {
+            "username": username,
+            "email": email,
+            "password": password,
+            "password_confirmation": password,
+        }
+    )
+    test_client.post(url_for("auth.register"), data=register_form_data)
+    login_form_data = form_data({"username_or_email": username, "password": password})
+    test_client.post(url_for("auth.login"), data=login_form_data)
+
+    return {"username": username, "email": email, "password": password}
