61f6d563d45f51e5abd5ba28049ee564e0fe2414
Dawid <dawid87872@gmail.com>
2024-03-30 13:47:52 +0100

    Remove category uuid from edit category form
    The uuid will be contained in the route

--- src/forms/categories/edit_category.py
+++ src/forms/categories/edit_category.py
@@ -6,13 +6,3 @@ class EditCategoryForm(AddCategoryForm):
     """
     Represents a form that edits a category. Contains all the fields required inside the add category form
     """
-
-    category_id = StringField(
-        name="category_uuid",
-        validators=[
-            validators.input_required(message="Category uuid is required"),
-            validators.length(
-                min=36, max=36, message="Category uuid must be a valid v4 uuid"
-            ),
-        ],
-    )
--- src/tests/test_forms/categories/test_edit_category_form.py
+++ src/tests/test_forms/categories/test_edit_category_form.py
@@ -1,4 +1,3 @@
-from uuid import uuid4
 from forms.categories import EditCategoryForm
 from tests.utils import form_data
 
@@ -7,29 +6,7 @@ def test_validates_proper_input():
     """
     Make sure that the form allows proper input
     """
-    data = form_data(
-        {"name": "School", "color": "#FFFFFF", "category_uuid": str(uuid4())}
-    )
-    form = EditCategoryForm(formdata=data)
-
-    assert form.validate()
-
-
-def test_does_not_validate_lack_of_category_uuid():
-    """
-    Make sure that the form does not validate a form without a category uuid
-    """
     data = form_data({"name": "School", "color": "#FFFFFF"})
     form = EditCategoryForm(formdata=data)
 
-    assert not form.validate()
-
-
-def test_does_not_validate_invalid_category_uuid():
-    """
-    Make sure that the form does not validate a form with an invalid uuid
-    """
-    data = form_data({"name": "School", "color": "#FFFFFF", "category_id": "in-va-lid-uuid"})
-    form = EditCategoryForm(formdata=data)
-
-    assert not form.validate()
+    assert form.validate()
