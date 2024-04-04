49af14b0188c0166676c5dec5d4e875d28b63c54
Dawid <dawid87872@gmail.com>
2024-04-04 22:07:51 +0200

    Add home icon

--- src/static/js/icon.js
+++ src/static/js/icon.js
@@ -347,6 +347,26 @@ class SquaresPlus extends Icon {
         '</svg>\n';
 }
 
+/**
+ * Represents a home icon
+ */
+class Home extends Icon {
+    static svgString =
+        '<svg\n' +
+        '    xmlns="http://www.w3.org/2000/svg"\n' +
+        '    fill="none"\n' +
+        '    viewBox="0 0 24 24"\n' +
+        '    stroke-width="1.5"\n' +
+        '    stroke="currentColor"\n' +
+        '>\n' +
+        '    <path\n' +
+        '        stroke-linecap="round"\n' +
+        '        stroke-linejoin="round"\n' +
+        '        d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"\n' +
+        '    />\n' +
+        '</svg>';
+}
+
 export {
     Identification,
     Lock,
@@ -364,4 +384,5 @@ export {
     Briefcase,
     Plus,
     SquaresPlus,
+    Home,
 };
