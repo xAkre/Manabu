3edd0afe7bd7092cd125c8f7d76aaf6b37d6b556
Dawid <dawid87872@gmail.com>
2024-04-04 05:52:03 +0200

    Add utilities for switching themes

---
+++ src/static/js/theme.js
@@ -0,0 +1,46 @@
+/**
+ * Load the user's preferred theme
+ */
+const initTheme = () => {
+    let prefersDarkScheme = localStorage.getItem('prefers-dark-scheme');
+
+    if (prefersDarkScheme === 'true') {
+        return document.documentElement.classList.add('dark');
+    }
+
+    prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (prefersDarkScheme.matches) {
+        document.documentElement.classList.add('dark');
+    }
+};
+
+/**
+ * Set the current theme
+ */
+const setTheme = (theme) => {
+    if (theme === 'light') {
+        localStorage.setItem('prefers-dark-scheme', 'false');
+        return document.documentElement.classList.remove('dark');
+    }
+
+    localStorage.setItem('prefers-dark-scheme', 'true');
+    document.documentElement.classList.add('dark');
+};
+
+/**
+ * Switch the current theme
+ */
+const switchTheme = () => {
+    const prefersDarkScheme = localStorage.getItem('prefers-dark-scheme');
+
+    if (prefersDarkScheme === 'true') {
+        return setTheme('dark');
+    } else if (prefersDarkScheme === 'false') {
+        return setTheme('light');
+    }
+
+    setTheme('light');
+};
+
+export { initTheme, setTheme, switchTheme };
