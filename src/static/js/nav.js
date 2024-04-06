33411ebf4994c4dd6d3aa81471d67b905a653ab3
Dawid <dawid87872@gmail.com>
2024-04-06 20:06:30 +0200

    Implement opening sidebar using nav

--- src/static/js/nav.js
+++ src/static/js/nav.js
@@ -1,4 +1,10 @@
 import { switchTheme } from './theme.js';
+import { openSidebar } from './sidebar.js';
 
 const switchThemeButton = document.querySelector('.switch-theme-button');
 switchThemeButton.addEventListener('click', switchTheme);
+
+const openSidebarButton = document.querySelector('.open-sidebar-button');
+if (openSidebarButton) {
+    openSidebarButton.addEventListener('click', openSidebar);
+}
