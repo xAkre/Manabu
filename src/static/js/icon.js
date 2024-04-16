000296628ec8bae313c200eb20934844cf722c7d
Dawid <dawid87872@gmail.com>
2024-04-16 18:56:23 +0200

    Add paint brush icon

--- src/static/js/icon.js
+++ src/static/js/icon.js
@@ -432,6 +432,26 @@ class Trash extends Icon {
         '</svg>';
 }
 
+/**
+ * Represents a paint brush icon
+ */
+class PaintBrush extends Icon {
+    static svgString =
+        '<svg\n' +
+        '    xmlns="http://www.w3.org/2000/svg"\n' +
+        '    fill="none"\n' +
+        '    viewBox="0 0 24 24"\n' +
+        '    stroke-width="1.5"\n' +
+        '    stroke="currentColor"\n' +
+        '>\n' +
+        '    <path\n' +
+        '        stroke-linecap="round"\n' +
+        '        stroke-linejoin="round"\n' +
+        '        d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42"\n' +
+        '    />\n' +
+        '</svg>';
+}
+
 export {
     Identification,
     Lock,
@@ -453,4 +473,5 @@ export {
     Cog,
     Pencil,
     Trash,
+    PaintBrush,
 };
