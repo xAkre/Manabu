3d527c1c975f802a990bc635910695a9f87278c2
Dawid <dawid87872@gmail.com>
2024-04-17 11:56:45 +0200

    Implement an edit category page

--- src/routes/categories/edit_category.py
+++ src/routes/categories/edit_category.py
@@ -1,6 +1,14 @@
 from typing import Any
-from flask import request, session as f_session, flash, url_for, redirect
+from flask import (
+    request,
+    session as f_session,
+    flash,
+    url_for,
+    redirect,
+    render_template,
+)
 from database import session as d_session
+from database.orm import select, and_
 from database.models import Category
 from database.exc import DatabaseError
 from forms.categories import EditCategoryForm
@@ -12,22 +20,10 @@ def edit_category(category_uuid: str) -> Any:
     """
     Handle editing categories
     """
-    if request.method == "GET":
-        return "Edit Category"
-
-    form = EditCategoryForm(request.form)
-
     # Check if category exists
     category = d_session.get(Category, category_uuid)
     if category is None:
         flash("There is no such category", "error")
-        return redirect(url_for("categories.show"))
-
-    # Validate user input
-    if not form.validate():
-        for field, errors in form.errors.items():
-            for error in errors:
-                flash(error, "error")
         return redirect(url_for("categories.show"))
 
     # Check if the category belongs to the current user
@@ -35,13 +31,60 @@ def edit_category(category_uuid: str) -> Any:
         flash("You do not have access to that category", "error")
         return redirect(url_for("categories.show"))
 
+    if request.method == "GET":
+        return render_template(
+            "pages/categories/edit_category.jinja",
+            user=f_session.get("user"),
+            category=category,
+            form=EditCategoryForm(),
+        )
+
+    form = EditCategoryForm(request.form)
+
+    # Validate user input
+    if not form.validate():
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(error, "error")
+        return render_template(
+            "pages/categories/edit_category.jinja",
+            user=f_session.get("user"),
+            category=category,
+            form=form,
+        )
+
+    # Check to make sure that the category name does not exist
+    existing_category = d_session.execute(
+        select(Category).where(
+            and_(
+                Category.uuid != category_uuid,
+                Category.name == form.name.data,
+                Category.user == f_session.get("user"),
+            )
+        )
+    ).scalar()
+
+    if existing_category is not None:
+        flash("A category with that name already exists", "error")
+        return render_template(
+            "pages/categories/edit_category.jinja",
+            user=f_session.get("user"),
+            category=category,
+            form=form,
+        )
+
     # Update the category
     try:
         category.name = form.name.data
         category.color = form.color.data
         d_session.commit()
         flash("Successfully updated category", "success")
-        return redirect(url_for("categories.edit", category_uuid=category.uuid))
+        return redirect(url_for("categories.show"))
     except DatabaseError:
         flash("There was an error while trying to edit the category", "error")
-        return redirect(url_for("categories.show"))
+        return render_template(
+            "pages/categories/edit_category.jinja",
+            user=f_session.get("user"),
+            category=category,
+            form=form,
+        )
