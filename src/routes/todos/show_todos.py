0ace8098c20456d558b72b828927365f43367a07
Dawid <dawid87872@gmail.com>
2024-04-28 14:45:49 +0200

    Show todo date and completed status in todo list

--- src/routes/todos/show_todos.py
+++ src/routes/todos/show_todos.py
@@ -1,7 +1,8 @@
 from typing import Any
+from datetime import date
 from flask import render_template, session as f_session
 from database import session as d_session
-from database.orm import select
+from database.orm import select, and_
 from database.models import Todo
 from middleware import require_login
 
@@ -11,9 +12,20 @@ def show_todos() -> Any:
     """
     Handle showing categories
     """
-    sorted_todos = d_session.scalars(
-        select(Todo).where(Todo.user == f_session.get("user")).order_by(Todo.due_date)
+    todos = d_session.scalars(
+        select(Todo)
+        .where(and_(Todo.user == f_session.get("user"), Todo.due_date >= date.today()))
+        .order_by(Todo.due_date)
     ).all()
+
+    sorted_dates = []
+    sorted_todos = []
+    for todo in todos:
+        if todo.due_date in sorted_dates:
+            sorted_todos[sorted_dates.index(todo.due_date)].append(todo)
+        else:
+            sorted_todos.append([todo])
+            sorted_dates.append(todo.due_date)
 
     return render_template(
         "pages/todos/show_todos.jinja",
